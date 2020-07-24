57a035400021e36467350563da4c9450065751a0
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-07-24 12:31:12 +0500

    add gradient on login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,40 +1,24 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget{
-
-  @override
-  LoginPageState createState() => LoginPageState();
-}
 
 
-class LoginPageState extends State<LoginPage>{
+class LoginPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.only(left: 8.0, right: 12.0, top: 60.0),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Image.asset(
-                          "assets/books.png", alignment: Alignment.topRight, height: 50, width: 50,
-                      ),
-                      Text("TheLearn - Учись, просто!", style: TextStyle(fontFamily: "Poppins-Bold", fontSize: 25, fontWeight: FontWeight.bold),)
-                    ],
-                  )
-                ],
-              ),
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            colors: [
+              Colors.orange[900],
+              Colors.orange[800],
+              Colors.orange[400]
+            ]
           )
-        ],
+        ),
       ),
     );
   }
