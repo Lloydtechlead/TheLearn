0f175760ea471af49ed7dfe073bb2844348f2454
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-07-24 12:01:05 +0500

    add logo text in login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class LoginPage extends StatefulWidget{
 
   @override
@@ -13,6 +12,30 @@ class LoginPageState extends State<LoginPage>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(left: 8.0, right: 12.0, top: 60.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Image.asset(
+                          "assets/books.png", alignment: Alignment.topRight, height: 50, width: 50,
+                      ),
+                      Text("TheLearn - Учись, просто!", style: TextStyle(fontFamily: "Poppins-Bold", fontSize: 25, fontWeight: FontWeight.bold),)
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
