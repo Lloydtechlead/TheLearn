477f0efaa9f422d76d69cdcb33da1a13ecdace7d
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-07-24 15:07:21 +0500

    change color and add menu in login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -9,15 +9,46 @@ class LoginPage extends StatelessWidget{
     // TODO: implement build
     return Scaffold(
       body: Container(
+        width: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.topCenter,
+            begin: Alignment.topRight,
             colors: [
-              Colors.orange[900],
-              Colors.orange[800],
-              Colors.orange[400]
+              Colors.pink[900],
+              Colors.pink[500],
+              Colors.pink[300]
             ]
           )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 50,),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    child: Column(
+                      children: <Widget>[
+                        Text('TheLearn - Учись, просто!', style: TextStyle(color: Colors.white, fontSize: 25))
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 15,)
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(40), topRight: Radius.circular(40))
+                ),
+              )
+            )
+          ],
         ),
       ),
     );
