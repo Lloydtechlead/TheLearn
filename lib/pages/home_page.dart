22d550cd8002186a3819441041eb773be5d37118
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-08-02 01:00:23 +0500

    add progressbar in home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,8 +13,17 @@ class _HomePageState extends State<HomePage>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(
-      child: Text('Главная'),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Image.asset('assets/home_page_progressbar.png', scale: 2.5),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
