c2e9b1e76326e967dfbd355cc849a17dae3ce090
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-08-03 00:37:46 +0500

    add ui new video

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 
 class HomePage extends StatefulWidget{
@@ -13,13 +14,49 @@ class _HomePageState extends State<HomePage>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       body: Container(
+        width: size.width,
+        height: size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            alignment: Alignment.topLeft,
+            image: ExactAssetImage("assets/home_page_progressbar.png", scale: 2.2),
+            fit: BoxFit.scaleDown
+          ),
+        ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
+            SizedBox(height: size.height / 1.7,),
             Container(
-              child: Image.asset('assets/home_page_progressbar.png', scale: 2.5),
+              height: size.height / 12,
+              width: size.width / 1.7,
+              margin: EdgeInsets.only(right: 15),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [BoxShadow(
+                      color: Color.fromRGBO(123, 104, 238, .2),
+                      blurRadius: 10,
+                  )]
+              ),
+              child: Center(
+                child: Text('Новые видео', style: TextStyle(fontFamily: 'DefaultFont', fontSize: 40)),
+              ),
+            ),
+            Container(
+              height: size.height / 4.75,
+              margin: EdgeInsets.only(left: 20, right: 10),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [BoxShadow(
+                    color: Color.fromRGBO(123, 104, 238, .2),
+                    blurRadius: 10,
+                  )]
+              ),
             )
           ],
         ),
