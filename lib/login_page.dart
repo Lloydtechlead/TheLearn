22c31a547e9414604667127858547960e389e2a1
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-08-03 23:20:36 +0500

    update ui login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -16,255 +16,181 @@ class LoginPage extends StatefulWidget{
 
 class _LoginPageState extends State<LoginPage>{
 
-
-
-  FirebaseAuth _auth = FirebaseAuth.instance;
-  FirebaseUser _user;
-  GoogleSignIn _googleSignIn = new GoogleSignIn();
-
-  String _email, _password;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-
     // TODO: implement build
+
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Container(
-        width: double.infinity,
+        height: size.height,
+        width: size.width,
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            colors: [
-              Colors.pink[900],
-              Colors.pink[500],
-              Colors.pink[300]
-            ]
+          image: DecorationImage(
+            image: AssetImage('assets/login_page_background.png'),
+            fit: BoxFit.cover
           )
         ),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 50,),
-            Padding(
-              padding: EdgeInsets.all(20),
+            SizedBox(height: size.height / 3 - 30),
+            Container(
+              margin: EdgeInsets.only(left: size.width / 3),
+              child: Text('Вход', style: TextStyle(fontFamily: 'DefaultFont', fontSize: 70)),
+            ),
+            SizedBox(height: 25),
+            Form(
+              key: _formKey,
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Container(
-                    child: Column(
-                      children: <Widget>[
-                        Text('TheLearn - Учись, просто!', style: TextStyle(color: Colors.white, fontSize: 25))
-                      ],
+                    padding: EdgeInsets.all(15),
+                    height: size.height / 11,
+                    width: size.width / 1.5,
+                    margin: EdgeInsets.only(left: size.width / 6),
+                    decoration: BoxDecoration(
+                      color: Colors.redAccent,
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Введите электронный адрес',
+                        hintStyle: TextStyle(fontFamily: 'DefaultFont', fontSize: 25)
+                      ),
                     ),
                   ),
-                  SizedBox(height: 15,)
+                  SizedBox(height: 25),
+                  Container(
+                    padding: EdgeInsets.all(15),
+                    height: size.height / 11,
+                    width: size.width / 1.5,
+                    margin: EdgeInsets.only(left: size.width / 6),
+                    decoration: BoxDecoration(
+                        color: Colors.redAccent,
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Введите пароль',
+                          hintStyle: TextStyle(fontFamily: 'DefaultFont', fontSize: 25)
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(40), topRight: Radius.circular(40))
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 10),
-                      Text('Вход в аккаунт', style: TextStyle(color: Colors.black, fontSize: 25),),
-                      SizedBox(height: 50),
-                      Container(
-                        padding: EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [BoxShadow(
-                            color: Color.fromRGBO(123, 104, 238, .3),
-                            blurRadius: 20,
-                            offset: Offset(0, 10)
-                          )]
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            Form(
-                              key: _formKey,
-                              child: Column(
-                                children: <Widget>[
-                                  TextFormField(
-                                    validator: (input) {
-                                      if(input.isEmpty){
-                                        return 'Введите вашу почту. Пример: thelearn@gmail.com';
-                                      }
-                                    },
-                                    decoration: InputDecoration(
-                                      hintText: 'Введите вашу почту'
-                                    ),
-                                    onSaved: (input) => _email = input,
-                                  ),
-                                  TextFormField(
-                                    validator: (input) {
-                                      if(input.isEmpty){
-                                        return 'Введите пароль';
-                                      }
-                                    },
-                                    obscureText: true,
-                                    decoration: InputDecoration(
-                                        hintText: 'Введите пароль'
-                                    ),
-                                    onSaved: (input) => _password = input,
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: 25),
-                      InkWell(
-                        child: Text('Забыли пароль?', style: TextStyle(color: Colors.grey, fontSize: 15)),
-                        onTap: () {},
-                      ),
-                      SizedBox(height: 35),
-                      InkWell(
-                        child: Container(
-                          width: 200,
-                          height: 60,
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(colors: [
-                              Colors.purple[200],
-                              Colors.purple[400],
-                              Colors.purple[700]
-                            ]),
-                            borderRadius: BorderRadius.circular(8),
-                            boxShadow: [BoxShadow(
-                                color: Colors.pink.withOpacity(.3),
-                                offset: Offset(0, 8),
-                                blurRadius: 8
-                            )],
-                          ),
-                          child: Material(
-                            color: Colors.transparent,
-                            child: InkWell(
-                              onTap: () {
-                                signInWithGoogle();
-                                print(isSignIn);
-                              },
-                              child: Center(
-                                child: Text('Войти', style: TextStyle(color: Colors.white, fontSize: 25)),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 45),
-                      Text('Вход через социальные сети', style: TextStyle(color: Colors.black, fontSize: 25)),
-                      SizedBox(height: 30),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          SocialIcon(
-                            colors: [
-                              Color(0xFF102397),
-                              Color(0xFF187adf),
-                              Color(0xFF00eaf8)
-                            ],
-                            iconData: CustomIcons.facebook,
-                            onPressed: () {
-                              _signInFacebook();
-                            },
-                          ),
-                          SocialIcon(
-                            colors: [
-                              Color(0xFFff4f38),
-                              Color(0xFFff355d),
-                            ],
-                            iconData: CustomIcons.googlePlus,
-                            onPressed: () {
-                              handleSignIn();
-                            },
-                          ),
-                          SocialIcon(
-                            colors: [
-                              Color(0xFF17ead9),
-                              Color(0xFF6078ea),
-                            ],
-                            iconData: CustomIcons.twitter,
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 32),
-                      InkWell(
-                        child: Text('Регистрация', style: TextStyle(color: Colors.deepPurple,fontSize: 15)),
-                        onTap: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
-                        },
-                      )
+            SizedBox(height: 130),
+            Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(left: size.width / 7.5),
+                  child: SocialIcon(
+                    colors: [
+                      Color(0xFF102397),
+                      Color(0xFF187adf),
+                      Color(0xFF00eaf8)
                     ],
+                    iconData: CustomIcons.facebook,
+                    onPressed: () {},
                   ),
                 ),
-              )
+                SizedBox(width: size.width / 2.8),
+                Container(
+                  child: SocialIcon(
+                    colors: [
+                      Color(0xFF17ead9),
+                      Color(0xFF6078ea),
+                    ],
+                    iconData: CustomIcons.twitter,
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(left: size.width / 2.3),
+              child: SocialIcon(
+                colors: [
+                  Color(0xFFff4f38),
+                  Color(0xFFff355d),
+                ],
+                iconData: CustomIcons.googlePlus,
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: <Widget>[
+                InkWell(
+                  child: Container(
+                    margin: EdgeInsets.only(left: 10),
+                    width: size.width / 2,
+                    height: size.height / 14,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        Colors.purple[200],
+                        Colors.purple[400],
+                        Colors.purple[700]
+                      ]),
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [BoxShadow(
+                          color: Colors.red.withOpacity(.3),
+                          offset: Offset(0, 8),
+                          blurRadius: 8
+                      )],
+                    ),
+                    child: Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        onTap: () {},
+                        child: Center(
+                          child: Text('Регистрация', style: TextStyle(fontFamily: 'DefaultFont',color: Colors.white, fontSize: 45)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: size.width / 9.5),
+                InkWell(
+                  child: Container(
+                    margin: EdgeInsets.only(right: 10),
+                    width: size.width / 3,
+                    height: size.height / 14,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        Colors.purple[200],
+                        Colors.purple[400],
+                        Colors.purple[700]
+                      ]),
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [BoxShadow(
+                          color: Colors.red.withOpacity(.3),
+                          offset: Offset(0, 8),
+                          blurRadius: 8
+                      )],
+                    ),
+                    child: Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        onTap: () {},
+                        child: Center(
+                          child: Text('Вход', style: TextStyle(fontFamily: 'DefaultFont', color: Colors.white, fontSize: 45)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             )
           ],
         ),
-      ),
+      )
     );
-  }
-
-  bool isSignIn = false;
-
-  Future<void> handleSignIn() async {
-    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
-    GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
-
-
-    AuthCredential credential = GoogleAuthProvider.getCredential(idToken: googleSignInAuthentication.idToken, accessToken: googleSignInAuthentication.accessToken);
-
-    AuthResult result = (await _auth.signInWithCredential(credential));
-
-    _user = result.user;
-    Navigator.push(context, MaterialPageRoute(builder: (context) => MainPage()));
-
-    setState(() {
-      isSignIn = true;
-    });
-  }
-
-  Future<void> googleSignout() async {
-    await _auth.signOut().then((onValue) {
-      _googleSignIn.signOut();
-      setState(() {
-        isSignIn = false;
-      });
-    });
-  }
-
-  Future<void> signInWithGoogle() async {
-    final formState = _formKey.currentState;
-    if(formState.validate()){
-      formState.save();
-      try{
-        AuthResult user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
-        Navigator.push(context, MaterialPageRoute(builder: (context) => MainPage()));
-      }catch(e){
-        print(e.message);
-      }
-    }
-  }
-
-  void _signInFacebook() async {
-    FacebookLogin facebookLogin = FacebookLogin();
-
-    final result = await facebookLogin.logIn(['email']);
-    final token = result.accessToken.token;
-    final graphResponse = await http.get(
-        'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${token}');
-    if(result.status == FacebookLoginStatus.loggedIn){
-      final credential = FacebookAuthProvider.getCredential(accessToken: token);
-      _auth.signInWithCredential(credential);
-    }
   }
 }
