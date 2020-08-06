e026cec7fbed4736fb12c29324711c49e245124d
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-08-06 13:36:17 +0500

    update users profile

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 
 class ProfilePage extends StatefulWidget{
@@ -8,14 +9,31 @@ class ProfilePage extends StatefulWidget{
   ProfilePage({Key key, @required this.userUid}) : super(key: key);
 
   @override
-  _ProfilePageState createState() => _ProfilePageState();
+  _ProfilePageState createState() => _ProfilePageState(userUid);
 }
 
 
 class _ProfilePageState extends State<ProfilePage>{
 
+  final String userUid;
+
+  _ProfilePageState(this.userUid);
+
+  String imageUrl = 'https://firebasestorage.googleapis.com/v0/b/thelearn.appspot.com/o/profile_images%2Fprofile_avatar.jpg?alt=media&token=7e2637b5-39ed-448c-8949-19386f31fc2e';
+
+  final firestoreInstance = Firestore.instance;
+
+
+
   @override
   Widget build(BuildContext context) {
+    firestoreInstance.collection("users").document(userUid).get().then((value) {
+      try {
+        imageUrl = value.data['photourl'];
+      } catch (_) {
+        null;
+      }
+    });
     Size size = MediaQuery.of(context).size;
     // TODO: implement build
     return Scaffold(
@@ -31,7 +49,13 @@ class _ProfilePageState extends State<ProfilePage>{
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: size.height * 0.15),
+            SizedBox(height: size.height * 0.13),
+            Container(
+              height: 100,
+              width: 100,
+              margin: EdgeInsets.only(left: size.width / 8),
+              child: Image.network(imageUrl),
+            ),
           ],
         )
       )
