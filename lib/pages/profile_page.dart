a76449e1b035c94ff907622ab1f2e793f49bbd80
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-08-16 15:47:53 +0500

    added rating on profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 
 class ProfilePage extends StatefulWidget{
@@ -22,6 +23,15 @@ class _ProfilePageState extends State<ProfilePage>{
 
   _ProfilePageState(this.nameValue, this.surnameValue, this.classValue, this.imageProfile);
 
+  Firestore firestoreInstance = Firestore.instance;
+
+  List ratingNames = [];
+  List videoNames = [];
+
+  @override
+  void initState() {
+    getRating();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,9 +71,57 @@ class _ProfilePageState extends State<ProfilePage>{
                 margin: classValue != null ? EdgeInsets.only(left: size.width / 8) : EdgeInsets.only(right: size.width / 1.5),
                 child: classValue != null ? Text(classValue, style: TextStyle(fontWeight: FontWeight.bold)) : SpinKitWave(color: Colors.black12 , size: 15)
             ),
+            SizedBox(height: size.height / 6),
+            Container(
+              margin: EdgeInsets.only(left: 40),
+              child: Text('Рейтинг по России', style: TextStyle(fontFamily: 'VideoFont', fontSize: 30))
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 10, right: 10, bottom: 20),
+              height: size.height / 3.5,
+              width: size.width / 1.05,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(8)
+              ),
+              child: Container(
+                margin: EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8)
+                ),
+                child: ListView.builder(
+                  itemCount: ratingNames.length,
+                  itemBuilder: (_, index) => Container(
+                    padding: EdgeInsets.only(bottom: 20, left: 15, right: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text('${index + 1} ${ratingNames[index]}', style: TextStyle(fontFamily: 'VideoFont', fontSize: 20)),
+                        Text('${videoNames[index]} Видео', style: TextStyle(fontFamily: 'VideoFont', fontSize: 20))
+                      ],
+                    )
+                  ),
+                )
+              ),
+            )
           ],
         )
       )
     );
+  }
+  
+  
+  void getRating() {
+    firestoreInstance.collection('rating').orderBy('viewed_video').getDocuments().then((result) {
+      result.documents.forEach((element) {
+        firestoreInstance.collection('users').document(element.documentID).get().then((value) {
+          setState(() {
+            ratingNames.add(value['name'] + ' ' + value['surname']);
+            videoNames.add(element.data['viewed_video']);
+          });
+        });
+      });
+    });
   }
 }
