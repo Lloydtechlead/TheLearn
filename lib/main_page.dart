49be8b80431cc966e737166af6ce506c35bc4cd1
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-08-22 00:34:37 +0500

    superior adaptability application

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -6,6 +6,7 @@ import 'pages/profile_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'pages/cribs.dart';
+import 'package:connectivity/connectivity.dart';
 
 
 class MainPage extends StatefulWidget{
@@ -39,79 +40,26 @@ class _HomePageState extends State<MainPage>{
 
   FirebaseMessaging firebaseMessaging = new FirebaseMessaging();
 
+  List _classData = [];
 
-  void getVideoLesson() {
 
-    List _classData = [];
+  void getVideoLesson() async {
 
-    firestoreInstance.collection('users').document(userUid).get().then((result) {
-      firestoreInstance.collection('video_lesson').document('class_${result.data['class']}').get().then((value) {
-        setState(() {
-          _classData = value.data.values.toList();
-          for(var i = 0; i < _classData.length; i++) {
-            lessonsNamesEn.add(_classData[i]);
-            if(_classData[i] == 'mathematics') {
-              lessonsNamesRu.add('Математика');
-            }else if(_classData[i] == 'russian_language') {
-              lessonsNamesRu.add('Русский язык');
-            }else if(_classData[i] == 'literature') {
-              lessonsNamesRu.add('Литература');
-            }else if(_classData[i] == 'surrounding_world') {
-              lessonsNamesRu.add('Окружающий мир');
-            }else if(_classData[i] == 'english_language') {
-              lessonsNamesRu.add('Английский язык');
-            }else if(_classData[i] == 'biology') {
-              lessonsNamesRu.add('Биология');
-            }else if(_classData[i] == 'geography') {
-              lessonsNamesRu.add('География');
-            }else if(_classData[i] == 'social studies') {
-              lessonsNamesRu.add('Обществознание');
-            }else if(_classData[i] == 'informatics') {
-              lessonsNamesRu.add('Информатика');
-            }else if(_classData[i] == 'algebra') {
-              lessonsNamesRu.add('Алгебра');
-            }else if(_classData[i] == 'geometry') {
-              lessonsNamesRu.add('Геометрия');
-            }else if(_classData[i] == 'physics') {
-              lessonsNamesRu.add('Физика');
-            }else if(_classData[i] == 'general history') {
-              lessonsNamesRu.add('Всеобщая история');
-            }else if(_classData[i] == 'history of Russia') {
-              lessonsNamesRu.add('История России');
-            }else if(_classData[i] == 'chemistry') {
-              lessonsNamesRu.add('Химия');
-            }else if(_classData[i] == 'life safety') {
-              lessonsNamesRu.add('ОБЖ');
-            }
-          }
-        });
-      });
-    });
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if(connectivityResult == ConnectivityResult.mobile) {
+      getLessonNames();
+    }else if(connectivityResult == ConnectivityResult.wifi) {
+      getLessonNames();
+    }else {
+      showAlert();
+    }
   }
 
   @override
   void initState() {
     super.initState();
     getVideoLesson();
-    firestoreInstance.collection('users').document(userUid).get().then((value) {
-      setState(() {
-        nameValue = value.data['name'];
-        surnameValue = value.data['surname'];
-        classValue = value.data['class'];
-        imageProfile = Image.network(value.data['photourl']);
-      });
-    });
-    firebaseMessaging.configure(
-      onMessage: (Map<String, dynamic> message) {
-        print(message);
-      },
-      onResume: (Map<String, dynamic> message) {
-        print(message);
-      },
-      onLaunch: (Map<String, dynamic> message) {
-        print(message);
-      }
-    );
+    getUserInfo();
   }
 
 
@@ -165,6 +113,120 @@ class _HomePageState extends State<MainPage>{
           });
         },
       ),
+    );
+  }
+
+  void getLessonNames() {
+    firestoreInstance.collection('users').document(userUid).get().then((result) {
+      firestoreInstance.collection('video_lesson').document('class_${result.data['class']}').get().then((value) {
+        setState(() {
+          _classData = value.data.values.toList();
+          for(var i = 0; i < _classData.length; i++) {
+            lessonsNamesEn.add(_classData[i]);
+            if(_classData[i] == 'mathematics') {
+              lessonsNamesRu.add('Математика');
+            }else if(_classData[i] == 'russian_language') {
+              lessonsNamesRu.add('Русский язык');
+            }else if(_classData[i] == 'literature') {
+              lessonsNamesRu.add('Литература');
+            }else if(_classData[i] == 'surrounding_world') {
+              lessonsNamesRu.add('Окружающий мир');
+            }else if(_classData[i] == 'english_language') {
+              lessonsNamesRu.add('Английский язык');
+            }else if(_classData[i] == 'biology') {
+              lessonsNamesRu.add('Биология');
+            }else if(_classData[i] == 'geography') {
+              lessonsNamesRu.add('География');
+            }else if(_classData[i] == 'social studies') {
+              lessonsNamesRu.add('Обществознание');
+            }else if(_classData[i] == 'informatics') {
+              lessonsNamesRu.add('Информатика');
+            }else if(_classData[i] == 'algebra') {
+              lessonsNamesRu.add('Алгебра');
+            }else if(_classData[i] == 'geometry') {
+              lessonsNamesRu.add('Геометрия');
+            }else if(_classData[i] == 'physics') {
+              lessonsNamesRu.add('Физика');
+            }else if(_classData[i] == 'general history') {
+              lessonsNamesRu.add('Всеобщая история');
+            }else if(_classData[i] == 'history of Russia') {
+              lessonsNamesRu.add('История России');
+            }else if(_classData[i] == 'chemistry') {
+              lessonsNamesRu.add('Химия');
+            }else if(_classData[i] == 'life safety') {
+              lessonsNamesRu.add('ОБЖ');
+            }
+          }
+        });
+      });
+    });
+  }
+
+  void getUserInfo() {
+    firestoreInstance.collection('users').document(userUid).get().then((value) {
+      setState(() {
+        nameValue = value.data['name'];
+        surnameValue = value.data['surname'];
+        classValue = value.data['class'];
+        imageProfile = Image.network(value.data['photourl']);
+      });
+    });
+    firebaseMessaging.configure(
+        onMessage: (Map<String, dynamic> message) {
+          print(message);
+        },
+        onResume: (Map<String, dynamic> message) {
+          print(message);
+        },
+        onLaunch: (Map<String, dynamic> message) {
+          print(message);
+        }
+    );
+  }
+
+  Future<void> showAlert() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Row(
+            children: <Widget>[
+              Icon(Icons.error),
+              Text('Ошибка подключения')
+            ],
+          ),
+          content: SingleChildScrollView(
+            child: Text('Пожалуйста, проверьте подключение к интернету'),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Row(
+                children: <Widget>[
+                  Text('Повторить'),
+                  Icon(Icons.refresh)
+                ],
+              ),
+              onPressed: () {
+                getVideoLesson();
+                getUserInfo();
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Row(
+                children: <Widget>[
+                  Text('Ок'),
+                  Icon(Icons.check_circle)
+                ],
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      }
     );
   }
 }
