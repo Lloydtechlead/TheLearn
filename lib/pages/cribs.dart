92bbc0c598120549f9a2bb860042289302e86f98
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-08-24 22:14:42 +0500

    update image container

--- lib/pages/cribs.dart
+++ lib/pages/cribs.dart
@@ -15,28 +15,34 @@ class CribsPageState extends State<CribsPage> {
       children: <Widget>[
         Container(
           margin: EdgeInsets.symmetric(horizontal: (MediaQuery.of(context).size.width - 200) / 2),
-          height: 170,
-          width: 200,
+          height: 190,
+          width: 210,
           child: Column(
             children: <Widget>[
               Container(
                 child: Image.asset('assets/crib_page_ventilator.gif', scale: 1.5),
                 alignment: Alignment.topRight,
               ),
+              SizedBox(width: 3),
               Container(
-                height: 120,
-                width: 200,
+                height: 140,
+                width: 210,
                 color: Colors.black,
                 child: Container(
                   margin: EdgeInsets.all(3),
                   color: Colors.grey[600],
-                  child: Container(
-                    margin: EdgeInsets.only(left: 14, top: 7, bottom: 7, right: 7),
-                    color: Colors.white,
-                    child: Center(
-                      child: Text('В будующих обновлениях!', style: TextStyle(fontFamily: 'VideoFont', fontSize: 15),),
-                    ),
-                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      Image.asset('assets/crib_page_picture.png'),
+                      Container(
+                        margin: EdgeInsets.only(left: 25, top: 7, bottom: 20, right: 0),
+                        color: Colors.white,
+                        child: Center(
+                          child: Text('В будующих обновлениях!', style: TextStyle(fontFamily: 'VideoFont', fontSize: 15),),
+                        ),
+                      ),
+                    ],
+                  )
                 )
               )
             ],
